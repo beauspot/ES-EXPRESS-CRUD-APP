ac84c8e77c2e724de7ede5cde5a6637c9a9d293b
beauspot <beauspot@outlook.com>
2023-03-22 19:25:47 +0100

    finished setting up the basics for the route and linking it to the task controllers.

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,4 +1,21 @@
+import {
+  getAllTasks,
+  createTask,
+  getASingleTask,
+  editTask,
+  deleteTask,
+} from "../controllers/taskCtrls.js";
 import express from "express";
 const router = express.Router();
+
+router.get("/getalltasks", getAllTasks);
+
+router.post("/createtask", createTask);
+
+router.get("/singletask/:id", getASingleTask);
+
+router.patch("/edittasks/:id", editTask);
+
+router.delete("/deletetasks/:id", deleteTask);
 
 export default router;
