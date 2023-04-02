8461fef3bf4f53b93e05cc25d8d7b69a3f54b63d
beauspot <beauspot@outlook.com>
2023-04-02 11:16:23 +0100

    modified the task model file by correcting the association Error.

--- model/taskModel.js
+++ model/taskModel.js
@@ -9,7 +9,7 @@ const TaskSchema = new mongoose.Schema(
     author: {
       type: mongoose.Schema.Types.ObjectId,
       required: false,
-      ref: "AuthModel",
+      ref: "authModels",
     },
     taskname: {
       type: String,
